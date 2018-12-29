7321194348e26a3914daf3e95941a56908eec0d3
Tyler Quiring <tylerdq@gmail.com>
2018-12-28 21:30:06 -0500

    Add dry run functionality to address issue #1

--- resize.py
+++ resize.py
@@ -1,7 +1,5 @@
-import click, os, errno, sys
+import click, os, errno, sys, io
 from PIL import Image
-
-cwd = os.getcwd()
 
 
 def mkdir(directory):
@@ -21,15 +19,21 @@ def cli():
 @click.argument('pixels', type=int)
 @click.option('--quality', '-q', default=75, type=click.IntRange(1, 95,
               clamp=True), help='Specify resized image quality (0-100).')
-def width(pixels, quality):
+@click.option('--dry', '-d', is_flag=True, help='Do a dry run (recommended to do first).')
+def width(pixels, quality, dry):
     """\b
     Resize image(s) proportionally, given desired width.
     Requires PIXELS as an integer greater than 0."""
     if pixels == 0:
         click.echo('PIXELS must be an integer greater than 0.')
         sys.exit()
-    dirname = 'w' + str(pixels) + 'q' + str(quality)
-    mkdir(dirname)
+    cwd = os.getcwd()
+    if dry:
+        sizes = []
+        x = 0
+    else:
+        dirname = 'w' + str(pixels) + 'q' + str(quality)
+        mkdir(dirname)
     for file in os.listdir(cwd):
         if file.lower().endswith(('.jpg', '.png', '.jpeg')):
             with Image.open(file) as image:
@@ -38,24 +42,41 @@ def width(pixels, quality):
                 newHeight = origHeight / origWidth * pixels
                 image = image.resize((int(newWidth), int(newHeight)),
                                      Image.ANTIALIAS)
-                os.chdir(dirname)
-                image.save(file, 'JPEG', quality=quality)
-                os.chdir(cwd)
+                if dry:
+                    x = x + 1
+                    output = io.BytesIO()
+                    image.save(output, 'JPEG', quality=quality)
+                    contents = int(len(output.getvalue()))
+                    output.close()
+                    print(file + ' Original=' + str(origWidth) + 'x' + str(origHeight) + ' Resized=' + str(newWidth) + 'x' + str(newHeight) + ', ' + '~' + str(round(contents/1000)) + 'kb')
+                    sizes.append(contents)
+                else:
+                    os.chdir(dirname)
+                    image.save(file, 'JPEG', quality=quality)
+                    os.chdir(cwd)
+    if dry:
+        print(str(x) + ' images totaling ' + '~' + str(round(sum(sizes)/1000)) + 'kb')
 
 
 @cli.command()
 @click.argument('pixels', type=int)
 @click.option('--quality', '-q', default=75, type=click.IntRange(1, 95,
               clamp=True), help='Specify resized image quality (0-100).')
-def height(pixels, quality):
+@click.option('--dry', '-d', is_flag=True, help='Do a dry run (recommended to do first).')
+def height(pixels, quality, dry):
     """\b
     Resize image(s) proportionally, given desired height.
     Requires PIXELS as an integer greater than 0."""
     if pixels == 0:
         click.echo('PIXELS must be an integer greater than 0.')
         sys.exit()
-    dirname = 'h' + str(pixels) + 'q' + str(quality)
-    mkdir(dirname)
+    cwd = os.getcwd()
+    if dry:
+        sizes = []
+        x = 0
+    else:
+        dirname = 'h' + str(pixels) + 'q' + str(quality)
+        mkdir(dirname)
     for file in os.listdir(cwd):
         if file.lower().endswith(('.jpg', '.png', '.jpeg')):
             with Image.open(file) as image:
@@ -64,35 +85,64 @@ def height(pixels, quality):
                 newWidth = origWidth / origHeight * pixels
                 image = image.resize((int(newWidth), int(newHeight)),
                                      Image.ANTIALIAS)
-                os.chdir(dirname)
-                image.save(file, 'JPEG', quality=quality)
-                os.chdir(cwd)
+                if dry:
+                    x = x + 1
+                    output = io.BytesIO()
+                    image.save(output, 'JPEG', quality=quality)
+                    contents = int(len(output.getvalue()))
+                    output.close()
+                    print(file + ' Original=' + str(origWidth) + 'x' + str(origHeight) + ' Resized=' + str(newWidth) + 'x' + str(newHeight) + ', ' + '~' + str(round(contents/1000)) + 'kb')
+                    sizes.append(contents)
+                else:
+                    os.chdir(dirname)
+                    image.save(file, 'JPEG', quality=quality)
+                    os.chdir(cwd)
+    if dry:
+        print(str(x) + ' images totaling ' + '~' + str(round(sum(sizes)/1000)) + 'kb')
 
 
 @cli.command()
 @click.argument('percentage', type=int)
 @click.option('--quality', '-q', default=75, type=click.IntRange(1, 95,
               clamp=True), help='Specify resized image quality (1-95).')
-def percent(percentage, quality):
+@click.option('--dry', '-d', is_flag=True, help='Do a dry run (recommended to do first).')
+def percent(percentage, quality, dry):
     """\b
     Resize image(s) to be a proportion of their original size.
     Requires PERCENTAGE as an integer greater than 0."""
     if percentage == 0:
         click.echo('PERCENTAGE must be an integer greater than 0.')
         sys.exit()
-    dirname = str(percentage) + 'pq' + str(quality)
-    mkdir(dirname)
+    cwd = os.getcwd()
+    if dry:
+        sizes = []
+        x = 0
+    else:
+        dirname = 'p' + str(percentage) + 'q' + str(quality)
+        mkdir(dirname)
     for file in os.listdir(cwd):
         if file.lower().endswith(('.jpg', '.png', '.jpeg')):
             with Image.open(file) as image:
                 origWidth, origHeight = image.size
-                newWidth = origWidth * percentage / 100
-                newHeight = origHeight * percentage / 100
-                image = image.resize((int(newWidth), int(newHeight)),
+                newWidth = int(round(origWidth * percentage / 100))
+                newHeight = int(round(origHeight * percentage / 100))
+                image = image.resize((newWidth, newHeight),
                                      Image.ANTIALIAS)
-                os.chdir(dirname)
-                image.save(file, 'JPEG', quality=quality)
-                os.chdir(cwd)
+                if dry:
+                    x = x + 1
+                    output = io.BytesIO()
+                    image.save(output, 'JPEG', quality=quality)
+                    contents = int(len(output.getvalue()))
+                    output.close()
+                    print(file + ' Original=' + str(origWidth) + 'x' + str(origHeight) + ' Resized=' + str(newWidth) + 'x' + str(newHeight) + ', ' + '~' + str(round(contents/1000)) + 'kb')
+                    sizes.append(contents)
+                else:
+                    os.chdir(dirname)
+                    image.save(file, 'JPEG', quality=quality)
+                    os.chdir(cwd)
+    if dry:
+        print(str(x) + ' images totaling ' + '~' + str(round(sum(sizes)/1000)) + 'kb')
+
 
 if __name__ == '__main__':
     cli()
